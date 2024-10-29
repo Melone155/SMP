9fb3ba40aeb3e365cc8c89bc9303328bb925a5ed
Melone155 <101599412+Melone155@users.noreply.github.com>
2024-10-29 03:56:44 +0100

    Fix /ec <Player name>

--- src/main/java/de/melone/sMP/CMD/CMD_ec.java
+++ src/main/java/de/melone/sMP/CMD/CMD_ec.java
@@ -2,6 +2,7 @@ package de.melone.sMP.CMD;
 
 import de.melone.sMP.SMP;
 import net.kyori.adventure.chat.SignedMessage;
+import net.kyori.adventure.text.minimessage.MiniMessage;
 import org.bukkit.Bukkit;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
@@ -13,22 +14,33 @@ import org.jetbrains.annotations.NotNull;
 
 public class CMD_ec implements CommandExecutor {
     @Override
-    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String s, @NotNull String[] args) {
-        Player player = (Player) sender;
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command cmd, @NotNull String label, @NotNull String[] args) {
+        if (!(sender instanceof Player)) {
+            sender.sendMessage("Dieser Befehl kann nur von einem Spieler verwendet werden.");
+            return true;
+        }
 
+        Player player = (Player) sender;
         if (args.length == 1) {
             Player target = Bukkit.getPlayer(args[0]);
 
             if (target == null) {
-                player.sendMessage("Der angegebene Spieler ist nicht online.");
+                player.sendMessage(MiniMessage.miniMessage().deserialize(SMP.prefix + "<gray>Der angegebene Spieler ist nicht online oder existiert nicht."));
+                return true;
+            }
+
+            if (!player.hasPermission("smp.ec")) {
+                player.sendMessage(MiniMessage.miniMessage().deserialize(SMP.prefix + SMP.noperms));
                 return true;
             }
 
             player.openInventory(target.getEnderChest());
+            player.sendMessage(MiniMessage.miniMessage().deserialize(SMP.prefix + "<gray>Du schaust nun in die Enderchest von <b><yellow>" + target.getName()));
+        } else {
+
+            player.openInventory(player.getEnderChest());
         }
 
-        player.openInventory(player.getEnderChest());
-
-        return false;
+        return true;
     }
 }
